6b676329805f017fb17d88632d0c922a5b219ad2
SergioRt1 <sergio200035@gmail.com>
2018-12-03 19:17:31 -0500

    first commit

---
+++ src/main/java/com/example/Lab92/Lab92Application.java
@@ -0,0 +1,12 @@
+package com.example.Lab92;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab92Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab92Application.class, args);
+	}
+}
