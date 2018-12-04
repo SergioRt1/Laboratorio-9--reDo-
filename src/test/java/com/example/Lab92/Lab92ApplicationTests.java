cf434d9804fbafef300943cd2461cf3980dd90ad
SergioRt1 <sergio200035@gmail.com>
2018-12-03 22:24:37 -0500

    Only web

--- src/test/java/com/example/Lab92/Lab92ApplicationTests.java
+++ src/test/java/com/example/Lab92/Lab92ApplicationTests.java
@@ -1,15 +1,6 @@
 package com.example.Lab92;
 
-import com.example.Lab92.model.Message;
-import com.example.Lab92.persitence.MessagePersistece;
-import com.example.Lab92.services.MessageException;
-import com.google.gson.Gson;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,28 +14,9 @@ import java.io.IOException;
 @SpringBootTest
 public class Lab92ApplicationTests {
 
-    @Autowired
-    MessagePersistece persistece;
-
     @Test
     public void contextLoads() {
     }
 
-    @Test
-    public void sendRegisters() throws MessageException, IOException {
-        String postUrl = "https://lab9arsw.herokuapp.com/messages";
-        Gson gson = new Gson();
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(postUrl);
-        Message message = new Message();
-        message.setMessage("Hola");
-        StringEntity postingString = new StringEntity(gson.toJson(message));
-        post.setEntity(postingString);
-        post.setHeader("Content-type", "application/json");
-        HttpResponse response = httpClient.execute(post);
-        System.out.println(response);
-//        Assert.assertArrayEquals();
-
-    }
 
 }
