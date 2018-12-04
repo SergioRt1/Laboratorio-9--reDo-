26851f0901b0543abe67b343671ef9dca0f0410a
SergioRt1 <sergio200035@gmail.com>
2018-12-03 22:21:53 -0500

    Only web

--- src/main/java/com/example/Lab92/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Lab92.repository;
-
-import com.example.Lab92.model.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MessageRepository extends MongoRepository<Message, String> {
-
-}
